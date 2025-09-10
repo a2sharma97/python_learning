94905bd89b40a53835797a1825eaa09c7690dade
Abhishek Sharma <a2sharma97@gmail.com>
2025-09-10 21:06:02 +0530

    suggest activity according to weather

---
+++ conditional_problems/weather_activity.py
@@ -0,0 +1,10 @@
+weather = (input("Enter the weather ")).lower()
+
+if weather == "sunny":
+    print("Go for a walk")
+elif weather == "rainy":
+    print("Read a book")
+elif weather == "snowy":
+    print("Build a snowman")
+else:
+    print("bad weather choice")
