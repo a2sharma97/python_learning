45340cd7a44a37085ac83df2be2f4ce324fd129e
Abhishek Sharma <a2sharma97@gmail.com>
2025-09-10 20:57:19 +0530

    fruit ripness

---
+++ conditional_problems/fruit_ripness.py
@@ -0,0 +1,10 @@
+color = (input("Enter the color ")).lower()
+
+if color == "green":
+    print("Banana is Unrip")
+elif color == "yellow":
+    print("Banana is Ripe")
+elif color == "brown":
+    print("Banana is Overripe")
+else:
+    print("wrong color")
