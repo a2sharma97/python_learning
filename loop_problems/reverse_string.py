0b31414204722e587a8b7a58297df8a97864af19
Abhishek Sharma <a2sharma97@gmail.com>
2025-09-11 17:42:33 +0530

    print reverse string

--- loop_problems/reverse_string.py
+++ loop_problems/reverse_string.py
@@ -1,4 +1,7 @@
 input_string = input()
 
-for i in range(10, 0):
-    print(i)
+reverse_string = ""
+for i in range(0, len(input_string)):
+   reverse_string += input_string[len(input_string) - 1 - i ]
+
+print("reverse of the string is: ", reverse_string)
